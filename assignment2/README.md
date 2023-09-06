277fdf1f557ba86ba254d6e16856d9d5f6eaac59
CodeBrained <cristianking128@gmail.com>
2023-09-05 21:31:32 -0400

    Created assignment 2 along with a README.md file

---
+++ assignment2/README.md
@@ -0,0 +1,5 @@
+# Assignment 2 - Version Control
+## Objective
+* Implement a recursive fibonacci sequence method and upload it to github.
+## Resources
+* [Recursive fibonacci method in Java](https://www.tutorialspoint.com/recursive-fibonacci-method-in-java)
---
+++ assignment2/src/assignment2/Fibonacci.java
@@ -0,0 +1,16 @@
+package assignment2;
+
+public class Fibonacci {
+	public static int fibonacciNthTerm(int n) {
+		if ((n == 0) || (n == 1))
+			return n;
+		else
+			return fibonacciNthTerm(n - 1) + fibonacciNthTerm(n - 2);
+	}
+	
+	public static void main(String[] args) {
+		int num = 10;
+		System.out.print("The " + num + "th number in the fibonacci sequence is: " );
+		System.out.println(fibonacciNthTerm(num));
+	}
+}
